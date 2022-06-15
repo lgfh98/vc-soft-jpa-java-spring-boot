9477a430e39dca49a327b0f805a1a6ce048f0a70
lgfh98 <lgfigueroah@outlook.com>
2022-06-15 17:30:12 -0500

    hola

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.domain.Student;
+import com.example.demo.repository.IStudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,23 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(IStudentRepository studentDao) {
+        return args -> {
+            Student student1 = new Student(
+                    "Luis",
+                    "Jones",
+                    "maria.jones@amigoscode.com",
+                    23
+            );
+            Student student2 = new Student(
+                    "Camilo",
+                    "Vivas",
+                    "camilo.vivas@amigoscode.com",
+                    22
+            );
+            studentDao.save(student1);
+            studentDao.save(student2);
+        };
+    }
 }
